a5ac2ec05bd3491d8e66c9081dc34675e13c3ac0
Hiteshree <73422564+Hiteshree277@users.noreply.github.com>
2021-03-17 16:01:00 +0530

    Add files via upload

---
+++ C36-Student-Activity-main/js/form.js
@@ -0,0 +1,31 @@
+class Form{
+    constructor()
+    {}
+    
+    display()
+    {
+        var title=createElement('h2');
+        title.html("Car Racing Game");
+        title.position(130,0);
+
+        var input=createInput("Name");
+        input.position(130,160);
+
+        var button=createButton("Play");
+        button.position(250,200);
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            playerCount=playerCount+1;
+            var name=input.value();
+
+            player.update(name);
+            player.updateCount(playerCount);
+
+            var greeting=createElement('h2');
+            greeting.html("Hello "+name);
+            greeting.position(130,160);
+            });
+    }
+}
